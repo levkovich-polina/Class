1c4162b319e0c1032d8f3f27bd98c0645e210ab6
levkovich-polina <levkovich.polina@gmail.com>
2023-12-23 12:10:31 +0300

    Done

---
+++ Class_Task1/Class_Task1/Class_Task1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Class_Task1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Введите количество строк массива: ");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            int[][] array = new int[count][];
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write($"Строка {i + 1}: ");
+                var elementsString = Console.ReadLine();
+                var elements = elementsString.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                array[i] = new int[elements.Length];
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    if (int.TryParse(elements[j], out int value))
+                    {
+                        array[i][j] = value;
+                    }
+                    else
+                    {
+                        array[i][j] = 0;
+                    }
+                }
+            }
+            Console.WriteLine("Ваш массив:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    Console.Write($"{array[i][j]} ") ;
+                }
+                Console.WriteLine();
+            }
+            int max;
+            int min;
+            int sum;
+            Console.WriteLine("Итог:");
+            for (int i = 0; i < count; i++)
+            {
+                Min(in array[i], out min);
+                Max(in array[i], out max);
+                Sum(in array[i], out sum);
+
+                Console.WriteLine($"Строка {i + 1}:  Минимальное значение - {min}, Максимальное значение - {max}, Сумма всех значений - {sum}");
+            }
+            Console.ReadLine();
+        }
+        static void Min(in int[] row, out int min)
+        {
+             min = 100000;
+            for (int i = 0; i < row.Length; i++)
+            {
+                int value = row[i];
+                if (value < min)
+                {
+                    min = value;
+                }
+            }
+        }
+        static  void Max(in int[] row, out int max)
+        {
+            max = -1000;
+            for (int i = 0; i < row.Length; i++)
+            {
+                int value = row[i];
+
+                if (value > max)
+                {
+                    max = value;
+                }
+            }
+        }
+         static void Sum(in int[] row, out int sum)
+        {
+             sum = 0;
+            for (int i = 0; i < row.Length; i++)
+            {
+                int value = row[i];
+
+                sum += value;
+            }
+        }
+    }
+}
---
+++ Class_Task2/Class_Task2/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Class_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество пекарей:");
+            int numBakers = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите количество курьеров:");
+            int numCouriers = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите количество заказов:");
+            int numOrders = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите размер склада:");
+            int storageSize = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            PizzaWorkers workers = new PizzaWorkers(numBakers, numCouriers);
+            PizzaShop pizzaShop = new PizzaShop(workers, storageSize);
+
+            for (int i = 1; i <= numOrders; i++)
+            {
+                pizzaShop.TakeOrder(i);
+                pizzaShop.ProcessOrders();
+                pizzaShop.PrintOrdersState();
+
+                Console.WriteLine();
+            }
+
+            pizzaShop.AnalyzeOrders();
+
+            Console.ReadLine();
+        }
+    }
+
+    class PizzaWorkers
+    {
+        public int numBakers { get; }
+        public int numCouriers { get; }
+
+        public PizzaWorkers(int numBakers, int numCouriers)
+        {
+            this.numBakers = numBakers;
+            this.numCouriers = numCouriers;
+        }
+    }
+
+    class PizzaShop
+    {
+        private PizzaWorkers workers;
+        private int storageSize;
+        private int currentStorage = 0;
+
+        public PizzaShop(PizzaWorkers workers, int storageSize)
+        {
+            this.workers = workers;
+            this.storageSize = storageSize;
+        }
+        //поступление заказа номер
+        public void TakeOrder(int orderNumber)
+        {
+            Console.WriteLine($"Поступил заказ №{orderNumber}");
+        }
+        //работа пекарей и курьеров
+        public void ProcessOrders()
+        {
+            for (int i = 1; i <= workers.numBakers; i++)
+            {
+                if (currentStorage == storageSize)
+                {
+                    Console.WriteLine($"Пекарь {i} ожидает свободного места на складе");
+                    continue;
+                }
+
+                Console.WriteLine($"Пекарь {i} готовит пиццу");
+
+                //временя на приготовление пиццы
+                System.Threading.Thread.Sleep(1000);
+
+                Console.WriteLine($"Пекарь {i} положил пиццу на склад");
+                currentStorage++;
+            }
+
+            for (int i = 1; i <= workers.numCouriers; i++)
+            {
+                if (currentStorage == 0)
+                {
+                    Console.WriteLine($"Курьер {i} ожидает готовых пицц на складе");
+                    continue;
+                }
+
+                Console.WriteLine($"Курьер {i} забрал пиццу со склада");
+
+                //временя на доставку пиццы
+                System.Threading.Thread.Sleep(1000);
+
+                Console.WriteLine($"Курьер {i} доставил пиццу клиенту");
+                currentStorage--;
+            }
+        }
+        //состояние заказа
+        public void PrintOrdersState()
+        {
+            Console.WriteLine($"Состояние заказов: {currentStorage} пицц на складе, {workers.numBakers} пекарей готовят, {workers.numCouriers} курьеров доставляют");
+        }
+        //выполнение заказа и рекомендации
+        public void AnalyzeOrders()
+        {
+            if (currentStorage < storageSize)
+            {
+                Console.WriteLine("Рекомендуется увеличить количество заказов");
+            }
+            else if (currentStorage == storageSize)
+            {
+                Console.WriteLine("Рекомендуется расширить склад");
+            }
+            else if (currentStorage > storageSize)
+            {
+                int extraPizzas = currentStorage - storageSize;
+                Console.WriteLine($"Рекомендуется продать {extraPizzas} пицц клиентам");
+            }
+        }
+    }
+}
